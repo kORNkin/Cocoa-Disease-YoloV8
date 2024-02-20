3c5e4e548c5c17a3dc164e184698cca817e71d4d
Supphakon Yimi <93123516+kORNkin@users.noreply.github.com>
2024-02-20 11:21:58 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,33 @@ from ultralytics import YOLO
 import requests
 import time
 
+import serial
+import string
+
+#GPS data from arduino
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+ser.reset_input_buffer()
+
 #Line notify
 def line(txt):
-    url = 'https://notify-api.line.me/api/notify'
-    token = 'FXkSbQJioSr5nZhb0HcZCIDz0zQdrltgmFcYgQ0o2YN'
-    headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
-
-    r = requests.post(url, headers=headers , data = {'message':txt})
-    print(r.text)
+	url = 'https://notify-api.line.me/api/notify'
+	token = 'FXkSbQJioSr5nZhb0HcZCIDz0zQdrltgmFcYgQ0o2YN'
+	headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
+	
+	#GPS data
+	while True:
+		line = ser.readline().decode('utf-8').rstrip()
+		if line.find("GPS") != -1:
+			txt += '\n'
+			txt += line
+			break
+	#Send line text
+	r = requests.post(url, headers=headers , data = {'message':txt})
+	print(r.text)
 
 #Predictation
 model = YOLO("best.pt")
-results = model.predict(source="0", stream=True, show=True, conf=0.8) # source already setup
+results = model.predict(source="0", stream=True, show=True, conf=0.7) # source already setup
 names = model.names
 
 for r in results:
